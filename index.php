5766c713915d4c40543e8d720cd86b7e254d122d
myISxy <617642980@qq.com>
2017-01-09 23:16:10 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+//$newname = "/up";
+//var_dump($_FILES);
+//$tmp_name = $_FILES['name']['tmp_name'];
+//var_dump($tmp_name);
+//$resule = move_uploaded_file($tmp_name , $newname);
+//var_dump($_SERVER);
+//var_dump($resule);
+var_dump($_FILES);
